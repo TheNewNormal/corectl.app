3b6a21f943246ae0df92935d120a17d5d1adac1a
Rimantas Mocevicius <rmocius@gmail.com>
2016-07-06 13:07:46 +0100

    v0.1.0

--- src/get_go_binaries/get_corectl.sh
+++ src/get_go_binaries/get_corectl.sh
@@ -7,7 +7,7 @@ CORECTL_VERSION=$(curl -Ss https://api.github.com/repos/TheNewNormal/corectl/rel
 
 # download latest version of corectl for macOS
 echo "Downloading corectl $CORECTL_VERSION for macOS"
-curl -L -o corectl.tar.gz https://github.com/TheNewNormal/corectl/releases/download/$CORECTL_VERSION/corectl-$CORECTL_VERSION-osx-amd64.tar.gz
+curl -L -o corectl.tar.gz https://github.com/TheNewNormal/corectl/releases/download/$CORECTL_VERSION/corectl-$CORECTL_VERSION-macOS-amd64.tar.gz
 tar xzvf corectl.tar.gz > /dev/null 2>&1
 rm -f corectl.tar.gz
 chmod +x *
